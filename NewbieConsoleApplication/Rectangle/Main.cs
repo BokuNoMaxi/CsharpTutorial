843590d4e62788690ae47f1589900789599925cd
BokuNoMaxi <simpson_homer@gmx.at>
2018-10-31 16:13:00 +0100

    Creating Tutorial C#
    creating tutorial project with hello world and an additional class with functions

---
+++ NewbieConsoleApplication/Rectangle/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle;
+
+namespace Rectangle
+{
+    class executeRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle R = new Rectangle();
+            R.setDetails();
+            R.displayValues();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NewbieConsoleApplication/Rectangle/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        //Variablen:
+        private double length;
+        private double width;
+
+        public void setDetails()
+        {
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double getFlaeche()
+        {
+            return length * width;
+        }
+
+        public void displayValues()
+        {
+            Console.Write("Länge{0}:", length);
+            Console.Write("Breite{0}:", width);
+            Console.Write("Fläche{0}:", getFlaeche());
+        }
+    }
+}
